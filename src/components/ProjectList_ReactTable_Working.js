d7fa129bebbbfa03c98757ae52dbc015699745c9
Sreekanth Vadassery <sreekanth.vadassery.s@gmail.com>
2022-05-15 21:19:58 +0530

    Client side pagination with React Table completed

--- src/components/ProjectList_ReactTable_Working.js
+++ src/components/ProjectList_ReactTable_Working.js
@@ -4,7 +4,7 @@ import MOCK_DATA from './MOCK_DATA.json'
 import BTable from 'react-bootstrap/Table';
 import {Button, Card} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faList, faPlusSquare,faAngleRight,faAngleLeft } from '@fortawesome/free-solid-svg-icons'
+import { faList, faPlusSquare,faAngleRight,faAngleLeft,faAnglesLeft,faAnglesRight } from '@fortawesome/free-solid-svg-icons'
 import { format } from 'date-fns'
 
 //Reference: https://www.youtube.com/playlist?list=PLC3y8-rFHvwgWTSrDiwmUsl4ZvipOw9Cz
@@ -52,14 +52,18 @@ const ProjectList_ReactTable = () => {
         canNextPage,
         canPreviousPage,
         pageOptions,
+        gotoPage,
+        pageCount,
+        setPageSize,
         state,
         prepareRow
     } = useTable({
         columns,
         data
+        //,initialState: {pageIndex : 0}
     },useSortBy,usePagination)
 
-    const {pageIndex} = state
+    const {pageIndex , pageSize} = state
   
     return (
 
@@ -116,15 +120,44 @@ const ProjectList_ReactTable = () => {
                 </BTable>
             </Card.Body>
             <Card.Footer style={{textAlign:"right"}}>
+                
                 <span>
                     <i>Page{' '} <strong> {pageIndex + 1} of {pageOptions.length}</strong>{'  '}</i>
                 </span>
-                <Button onClick={ () => previousPage()} disabled={!canPreviousPage} size="sm" variant="info" type="button">
-                    <b><FontAwesomeIcon icon={faAngleLeft} />Previuos</b>
+                <span>
+                    | Go to page: {' '}
+                    <input className="bg-dark text-white border border-white rounded-pill" type='number' defaultValue={pageIndex + 1}
+                        onChange={ e => {
+                            const pageNumber = e.target.value ? Number(e.target.value) - 1 : 0
+                            gotoPage(pageNumber)
+                        }}
+                        style={{width: '60px'}}
+                    />
+                </span>
+                {' '}
+                <select value={pageSize} onChange={e => setPageSize(Number(e.target.value))} className="bg-dark text-white border border-white">
+                    {
+                        [10,25,50,100].map(pageSize =>(
+                            <option key={pageSize} value={pageSize}>
+                                Show {pageSize}
+                            </option>
+                        ))
+                    }
+                </select>
+                 {' '}
+                <Button onClick={ () => gotoPage(0)} disabled={!canPreviousPage} size="sm" variant="secondary" type="button" className='rounded-pill'>
+                    <b><FontAwesomeIcon icon={faAnglesLeft} /> First</b>
                 </Button>{' '}
-                <Button onClick={ () => nextPage()} disabled={!canNextPage} size="sm" variant="info" type="button" >
-                    <b>Next<FontAwesomeIcon icon={faAngleRight} /></b>
-                </Button>                
+                <Button onClick={ () => previousPage()} disabled={!canPreviousPage} size="sm" variant="secondary" type="button" className='rounded-pill'>
+                    <b><FontAwesomeIcon icon={faAngleLeft} /> Previuos</b>
+                </Button>{' '}
+                <Button onClick={ () => nextPage()} disabled={!canNextPage} size="sm" variant="secondary" type="button" className='rounded-pill'>
+                    <b>Next <FontAwesomeIcon icon={faAngleRight} /></b>
+                </Button> {' '}   
+                <Button onClick={ () => gotoPage(pageCount - 1)} disabled={!canNextPage} size="sm" variant="secondary" type="button" className='rounded-pill'>
+                    <b>Last <FontAwesomeIcon icon={faAnglesRight} /></b>
+                </Button>   
+                
             </Card.Footer>
             <br></br>
             <br></br>
