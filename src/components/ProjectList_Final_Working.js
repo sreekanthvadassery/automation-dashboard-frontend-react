0d128f52ded8dcfe111ea304cfde094e2c876919
Sreekanth Vadassery <sreekanth.vadassery.s@gmail.com>
2022-05-18 22:46:22 +0530

    Delete working

--- src/components/ProjectList_Final_Working.js
+++ src/components/ProjectList_Final_Working.js
@@ -1,32 +1,29 @@
-import React ,{useEffect, useState, useMemo} from 'react'
+import React ,{useEffect} from 'react'
 import { useTable,usePagination } from 'react-table';
 import { useQuery } from 'react-query';
 import { QueryClientProvider, QueryClient } from 'react-query';
 import BTable from 'react-bootstrap/Table';
-import {Button, Card, ButtonGroup,Alert} from 'react-bootstrap';
+import {Button, Card, ButtonGroup} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faList, faPlusSquare,faAngleRight,faAngleLeft,faAnglesLeft,faAnglesRight,faEdit,faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faList, faPlusSquare,faAngleRight,faAngleLeft,faAnglesLeft,faAnglesRight,faEdit } from '@fortawesome/free-solid-svg-icons'
 import { format } from 'date-fns'
 import {AiOutlineCheck,AiOutlineClose} from 'react-icons/ai'
-import {useLocation} from 'react-router-dom';
-import ProjectService from '../services/ProjectService';
-import DeleteConfirmation from './DeleteConfirmation';
+
 
 const queryClient = new QueryClient();
 
-
-/*const columns = [
+const columns = [
     {
-        Header: 'Id',
-        accessor: 'projectId',
+      Header: 'Id',
+      accessor: 'projectId',
     },
     {
-        Header: 'Project Name',
-        accessor: 'projectName',
+      Header: 'Project Name',
+      accessor: 'projectName',
     },
     {
-        Header: 'Description',
-        accessor: 'projectDescription',
+      Header: 'Description',
+      accessor: 'projectDescription',
     },
     {
         Header: 'Enabled',
@@ -52,11 +49,11 @@ const queryClient = new QueryClient();
         Cell: row => (
             <ButtonGroup>
                 <Button href={`/edit-project/${row.row.original.projectId}`} size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit} /></Button>{' '}
-                <Button onClick={() => showDeleteModal("Project", row.row.original.projectId)} size="sm" variant="outline-danger"><FontAwesomeIcon icon={faTrash} /></Button>
+                
             </ButtonGroup>
         )
     },
-];*/
+];
 
 
 const trimData = (data = []) =>
@@ -108,70 +105,6 @@ const reducer = (state, { type, payload }) => {
 //         : https://dev.to/elangobharathi/server-side-pagination-using-react-table-v7-and-react-query-v3-3lck
 const ProjectList_Final = () => {
 
-    const COLUMNS = [
-        {
-            Header: 'Id',
-            accessor: 'projectId',
-        },
-        {
-            Header: 'Project Name',
-            accessor: 'projectName',
-        },
-        {
-            Header: 'Description',
-            accessor: 'projectDescription',
-        },
-        {
-            Header: 'Enabled',
-            accessor: 'enabled',
-            Cell: ({ value }) => {return value? <div style={{ textAlign: "center" }}><AiOutlineCheck/></div> : <div style={{ textAlign: "center" }}><AiOutlineClose/></div>}
-        },
-        {
-            Header: 'Created At',
-            accessor: 'createdAt',
-            Cell: ({ value }) => {return format(new Date(value),'dd/MM/yyyy HH:mm:ss')}
-        },
-        {
-            Header: 'Updated At',
-            accessor: 'updatedAt',
-            Cell: ({ value }) => {return format(new Date(value),'dd/MM/yyyy HH:mm:ss')}
-        },
-        {
-            Header: 'Last Updated By',
-            accessor: 'lastUpdatedBy',
-        },
-        {
-            Header: 'Actions',
-            Cell: row => (
-                <ButtonGroup>
-                    <Button href={`/edit-project/${row.row.original.projectId}`} size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit} /></Button>{' '}
-                    <Button onClick={() => showDeleteModal("Project", row.row.original.projectId)} size="sm" variant="outline-danger"><FontAwesomeIcon icon={faTrash} /></Button>
-                </ButtonGroup>
-            )
-        },
-    ];
-
-    //useLocation is used to get the data passed via useNavigate()
-    //https://stackoverflow.com/questions/52238637/react-router-how-to-pass-data-between-pages-in-react
-    const loc  = useLocation();
-
-    //this useEffect is used to load the data in list (on page load this will be called)
-    useEffect(() => {
-        //When we are saving or updating any value, we are passing a state 'dataSubmittedSuccessMessage' along with useNavigate() function
-        //Using useLocation hook we can get the state passed via useNavigate() function
-        //We are reading the state and assigning it to the successMessage state available in this component
-        //we need to set the state if and only if loc.state is present
-        if(loc.state){
-            //setting the data to existing successMessage state
-            setSuccessMessage(loc.state.dataSubmmittedSuccessMessage)
-            //Once set the data clear it so that during page refresh the message will be disappeared
-            //https://stackoverflow.com/questions/53278986/how-to-clear-props-location-state-on-refresh
-            window.history.replaceState(null, '')
-        }
-        //calling the method for getting the list of Projects
-        getProjectData(queryPageIndex,queryPageSize);
-    }, [loc.state])
-
     const getProjectData = async (page, pageSize) => {
         console.log(`http://localhost:8080/api/v1/project/find-all?page=${page}&size=${pageSize}`)
         try {
@@ -189,59 +122,6 @@ const ProjectList_Final = () => {
         }
     };
 
-    //method for deleting the project
-    const deleteProject = (projectId) =>{
-        console.log('Delete Id: '+projectId);
-        ProjectService.deleteProject(projectId).then((response) =>{
-            //call the method which returns all the project details
-            getProjectData(queryPageIndex,queryPageSize);
-        }).catch(error =>{
-            console.log(error);
-        })
-    }
-
-    // DELETE CONFIRM MODAL CODE START//
-    // Set up some additional local states for handling the Delete Confirmation Modal dialog box
-    const [entityType, setEntityType] = useState(null);
-    const [id, setId] = useState(null);
-    const [displayConfirmationModal, setDisplayConfirmationModal] = useState(false);
-    const [deleteMessage, setDeleteMessage] = useState(null);
-    const [successMessage, setSuccessMessage] = useState(null);
-
-    // Handle the displaying of the Modal based on entityType and id
-    const showDeleteModal = (entityType, id) => {
-        setEntityType(entityType);
-        setId(id);
-        //Setting the success message to null (Once the delete is completed, then only we need the success message)
-        setSuccessMessage(null);
-        //Setting the message which needs to be displayed inside Modal
-        
-        //setDeleteMessage(`Are you sure you want to delete the ${entityType} '${projects.find((x) => x.projectId === id).projectName}' with ID: ${id}?`);
-        setDeleteMessage(`Are you sure you want to delete the ${entityType} `);
-        
-        //We are setting this variable to true and pass it to the Modal so that it will show the Modal
-        setDisplayConfirmationModal(true);
-    };
-
-    // Hide the modal
-    const hideConfirmationModal = () => {
-        setDisplayConfirmationModal(false);
-    };
-
-    // Handle the actual deletion of the item
-    const submitDelete = (entityType, id) => {
-        //Setting the success message
-        //setSuccessMessage(`The ${entityType} '${projects.find((x) => x.projectId === id).projectName}' was deleted successfully.`);
-
-        setSuccessMessage(`The ${entityType}  was deleted successfully.`);
-
-        //Calling the actual delete method
-        deleteProject(id);
-        //After calling the delete method, we need to close the Modal    
-        setDisplayConfirmationModal(false);
-    };
-    // DELETE CONFIRM MODAL CODE END//
-
     const [{ queryPageIndex, queryPageSize, totalCount }, dispatch] =
         React.useReducer(reducer, initialState);
 
@@ -254,8 +134,6 @@ const ProjectList_Final = () => {
         }
     );
 
-    const columns = useMemo(() => COLUMNS,[])
-    
     const {
         getTableProps,
         getTableBodyProps,
@@ -330,7 +208,6 @@ const ProjectList_Final = () => {
                 </Button>
             </Card.Header>
             <Card.Body>
-                {successMessage && <Alert variant="success" align="center">{successMessage}</Alert>}
                 <BTable bordered hover striped variant="dark" {...getTableProps()}>
                     <thead>
                         {
@@ -410,7 +287,6 @@ const ProjectList_Final = () => {
             </Card.Footer>
             <br></br>
             <br></br>
-            <DeleteConfirmation showModal={displayConfirmationModal} hideModal={hideConfirmationModal} confirmModal={submitDelete} id={id} entityType={entityType} message={deleteMessage} />
         </Card>    
     )
 }
